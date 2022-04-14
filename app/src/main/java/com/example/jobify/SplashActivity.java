29ce1c3e54bdb600b91aac2511e867e3fca71399
007Ackerman <srivastavaraj53@gmail.com>
2022-04-15 00:31:02 +0530

    commit

--- app/src/main/java/com/example/jobify/SplashActivity.java
+++ app/src/main/java/com/example/jobify/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.jobify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 3;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, signup.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
     }
 }
