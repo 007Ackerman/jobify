15941ec92b41db83f3a45ab26f3c5c50522c556d
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-04-15 21:03:37 +0530

    Designing Deshboard

--- app/src/main/java/com/example/jobify/SignINActivity.java
+++ app/src/main/java/com/example/jobify/SignINActivity.java
@@ -3,60 +3,151 @@ package com.example.jobify;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.example.jobify.AdminDashboardActivity;
+import com.example.jobify.databinding.ActivitySigninBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class SignINActivity extends AppCompatActivity {
 
-    FirebaseAuth auth;
-    EditText email,pass;
-    Button login;
+
+    //view binding
+    private ActivitySigninBinding binding;
+
+    //firebase Auth
+    private FirebaseAuth firebaseAuth;
+
+    //progressbar
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
-        getSupportActionBar().hide();
+        binding= ActivitySigninBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //init firebase Auth
+        firebaseAuth=FirebaseAuth.getInstance();
 
 
-        auth= FirebaseAuth.getInstance();
-        email=findViewById(R.id.email);
-        pass=findViewById(R.id.pass);
-        login=findViewById(R.id.button);
-        login.setOnClickListener(new View.OnClickListener() {
+
+
+       /* binding.noAccountTv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });*/
 
-                String lemail,lpass;
-                lemail=email.getText().toString();
-                lpass=pass.getText().toString();
+        //handel click , begin login
+        binding.loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateData();
+            }
+        });
 
-                auth.signInWithEmailAndPassword(lemail,lpass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    }
+
+    private String email="" ,password ="";
+    private void validateData() {
+        email=binding.email.getText().toString();
+        password=binding.pass.getText().toString();
+
+
+        //Vaildate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(SignINActivity.this, "Invalid email Pattern.....", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(SignINActivity.this, "Enter Password....!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            //data is vaildated begin login
+            loginuser();
+        }
+
+    }
+
+    private void loginuser() {
+
+
+        //login User
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            startActivity(new Intent(SignINActivity.this, DashBoardActivity.class));
-                            Toast.makeText(SignINActivity.this,"Logged In",Toast.LENGTH_SHORT).show();
+                    public void onSuccess(AuthResult authResult) {
+                        //login successful ,check if user is user or Admin
+                        checkuser();
 
-                        }else
-                        {
-                            Toast.makeText(SignINActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(SignINActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+    private void checkuser() {
+
+
+
+
+        // check if user is user or Admin
+        //get current user
+        FirebaseUser firebaseUser =firebaseAuth.getCurrentUser();
+
+        //check in db
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(firebaseUser.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+
+                        //get user type
+                        String userType= ""+snapshot.child("userType").getValue();
+
+                        //check user type
+                        if(userType.equals("user")){
+                            //this is the simple user open the user Dashboard
+                            startActivity(new Intent(SignINActivity.this,DashBoardActivity.class));
+                            finish();
+
+                        }else if(userType.equals("admin")){
+                            //this the Admin , Open the Admin Dashboard
+                            startActivity(new Intent(SignINActivity.this, AdminDashboardActivity.class));
+                            finish();
                         }
-                        }
-                    });
-                }
+                    }
 
-            });
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
+                    }
+                });
     }
 }
