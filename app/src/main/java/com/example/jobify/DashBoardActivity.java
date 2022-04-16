dc74fbf98065a439e69396c6370d99b761d7936e
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-04-16 15:43:25 +0530

    Uploading jobs from admin side (UI)

--- app/src/main/java/com/example/jobify/DashBoardActivity.java
+++ app/src/main/java/com/example/jobify/DashBoardActivity.java
@@ -2,14 +2,60 @@ package com.example.jobify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+
+import com.example.jobify.databinding.ActivityDashboardBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DashBoardActivity extends AppCompatActivity {
+
+    //binding
+    private  ActivityDashboardBinding binding;
+
+    //Firebase Auth
+    private FirebaseAuth firebaseAuth;
+
+    //Shimer
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        binding=ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        //Hiding the tool bar
         getSupportActionBar().hide();
+
+        //init firebase Auth
+        firebaseAuth=FirebaseAuth.getInstance();
+
+
+        binding.notif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                checkUser();
+
+
+
+            }
+        });
+    }
+
+    private void checkUser() {
+        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+
+        if(firebaseUser==null){
+
+            //not login , goto main screen
+            startActivity(new Intent(DashBoardActivity.this,IntroActivity.class));
+            finish();
+        }
     }
 }
