ff491d4ff49a9e91348376a1b8cd9056cf996507
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-04-17 20:24:39 +0530

    Job Details

---
+++ app/src/main/java/com/example/jobify/JobDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.jobify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JobDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_details);
+    }
+}
